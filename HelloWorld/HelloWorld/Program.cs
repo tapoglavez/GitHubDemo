9d7ce860b21f0c58261042f23e12fe2ef7e82bfd
tapoglavez <ivanivanov_2003@abv.bg>
2017-06-24 15:49:19 +0300

    save-to na hello sc

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Hello C#!");
         }
     }
 }
